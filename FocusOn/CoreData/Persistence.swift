009bc835ce6b07e6b838a0d7b13b91bfe24339b5
melnelen <melnelen@gmail.com>
2022-03-25 21:24:20 +0100

    reorganizing

---
+++ FocusOn/CoreData/Persistence.swift
@@ -0,0 +1,26 @@
+//
+//  Persistence.swift
+//  FocusOn
+//
+//  Created by Alexandra Ivanova on 23/12/2021.
+//
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    let container: NSPersistentCloudKitContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentCloudKitContainer(name: "FocusOn")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+    }
+}
