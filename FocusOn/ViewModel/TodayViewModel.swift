1b6d0a5a7e19990a026597ae50a5618c535f7a1a
melnelen <melnelen@gmail.com>
2022-05-28 18:18:44 +0200

    remove extra func

--- FocusOn/ViewModel/TodayViewModel.swift
+++ FocusOn/ViewModel/TodayViewModel.swift
@@ -86,10 +86,6 @@ class TodayViewModel: ObservableObject {
             }
         }
     }
-
-    func checkLength(of text: String) -> Bool {
-        return text.count > 2 ? true : false
-    }
 }
 
 // MARK: TRASH
