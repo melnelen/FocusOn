3b20aa2fa20355f67c06f4f6afdfe10a3553d3e4
Alex Ivanova <melnelen@gmail.com>
2023-11-26 19:31:11 +0100

    (in progress) adding date and summary to the history view

--- FocusOn/View/HistoryView.swift
+++ FocusOn/View/HistoryView.swift
@@ -15,24 +15,61 @@ struct HistoryView: View {
     var body: some View {
         Section {
             if let goals = allGoals { // [Goal(), Goal(), Goal()]
-                List {
-                    ForEach(goals, id: \.self) { goal in
-                        Section {
-                            HStack {
-                                Text(goal.name)
-                                Spacer()
-                                Image(systemName: (goal.isCompleted ? "checkmark.seal.fill" : "xmark.octagon.fill"))
-                                    .foregroundColor(goal.isCompleted ? Color("SuccessColor") : Color("FailColor"))
-                            }.font(.system(size: 25))
-                            ForEach(Array(goal.tasks), id: \.self) { task in
-                                HStack {
-                                    Text(task.name)
-                                    Spacer()
-                                    Image(systemName: (task.isCompleted ? "checkmark.circle.fill" : "xmark.circle.fill"))
-                                        .foregroundColor(task.isCompleted ? Color("SuccessColor") : Color("FailColor"))
+                VStack {
+                    //                    Image(systemName: "text.badge.checkmark")
+                    //                        .font(.system(size: 50, weight: .bold))
+                    //                        .foregroundColor(Color("SuccessColor"))
+                    //                        .padding(.bottom)
+                    //                    Text("You have completed \(goals.filter { $0.isCompleted }.count) out of \(goals.count) goals.")
+                    //                }
+                    
+                    // Display monthly summaries and goals
+                    ForEach(viewModel.monthlySummaries.sorted(by: { $0.key > $1.key }), id: \.key) { (month, summary) in
+                        // Monthly Summary
+                        VStack {
+                            Text("\(month)")
+                                .font(.title)
+                                .foregroundColor(Color("AccentColor"))
+                                .padding(.vertical, 10)
+                            Text("\(summary)")
+                                .font(.headline)
+                                .foregroundColor(Color("AccentColor"))
+                                .padding(.vertical, 5)
+                                .fixedSize(horizontal: false, vertical: true)
+                        }
+                        
+                        // Goals for the month
+                        ForEach(viewModel.allGoals?.filter { goal in
+                            let dateFormatter = DateFormatter()
+                            dateFormatter.dateFormat = "MMMM yyyy"
+                            return dateFormatter.string(from: goal.createdAt) == month
+                        } ?? [], id: \.self) { goal in
+                            Section {
+                                // Goal with date
+                                VStack(alignment: .leading) {
+                                    Text("\(formattedDate(from: goal.createdAt))")
+                                        .font(.caption)
+                                        .foregroundColor(Color("AccentColor"))
+                                    HStack {
+                                        Text("\(goal.name)")
+                                            .font(.system(size: 25))
+                                        Spacer()
+                                        Image(systemName: (goal.isCompleted ? "checkmark.seal.fill" : "xmark.octagon.fill"))
+                                            .foregroundColor(goal.isCompleted ? Color("SuccessColor") : Color("FailColor"))
+                                    }
+                                }
+                                
+                                // Tasks
+                                ForEach(Array(goal.tasks), id: \.self) { task in
+                                    HStack {
+                                        Text("\(task.name)")
+                                        Spacer()
+                                        Image(systemName: (task.isCompleted ? "checkmark.circle.fill" : "xmark.circle.fill"))
+                                            .foregroundColor(task.isCompleted ? Color("SuccessColor") : Color("FailColor"))
+                                    }
                                 }
                             }
-                        } // header: { }
+                        }
                     }
                 }
             } else {
@@ -52,6 +89,12 @@ struct HistoryView: View {
 extension HistoryView {
     private func fetchCompletedGoals() {
         allGoals = viewModel.fetchGoals()
+    }
+    
+    private func formattedDate(from date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd MMM yyyy"
+        return dateFormatter.string(from: date)
     }
 }
 
--- FocusOn/ViewModel/HistoryViewModel.swift
+++ FocusOn/ViewModel/HistoryViewModel.swift
@@ -9,6 +9,7 @@ import Foundation
 
 class HistoryViewModel: ObservableObject {
     @Published var allGoals: [Goal]?
+    @Published var monthlySummaries: [String: String] = [:]
     private let dataService: DataServiceProtocol
     
     init( dataService: DataServiceProtocol = MockDataService()) {
@@ -18,6 +19,26 @@ class HistoryViewModel: ObservableObject {
     
     func fetchGoals() -> [Goal]? {
         allGoals = dataService.fetchGoals()
+        calculateMonthlySummaries()
         return allGoals
     }
+    
+    private func calculateMonthlySummaries() {
+            guard let goals = allGoals else { return }
+
+            // Group goals by month
+            let groupedGoals = Dictionary(grouping: goals) { goal in
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "MMMM yyyy"
+                return dateFormatter.string(from: goal.createdAt)
+            }
+
+            // Calculate summaries
+            for (month, goals) in groupedGoals {
+                let completedCount = goals.filter { $0.isCompleted }.count
+                let totalCount = goals.count
+                let summary = "\(completedCount) out of \(totalCount) goals completed"
+                monthlySummaries[month] = summary
+            }
+        }
 }
