8db6f3ddce241ea75318c233a712e0eab15fcf52
Alex Ivanova <melnelen@gmail.com>
2024-01-19 23:13:20 +0100

    LegendView

--- FocusOn/View/ProgressView.swift
+++ FocusOn/View/ProgressView.swift
@@ -58,58 +58,6 @@ struct ProgressView: View {
     }
 }
 
-private struct LegendView: View {
-    var body: some View {
-        Text("Legend:")
-            .foregroundColor(.accentColor)
-            .font(.headline)
-            .padding()
-        HStack(alignment: .top) {
-            VStack(alignment: .leading) {
-                HStack {
-                    Circle()
-                        .fill(Color("AlternateColor"))
-                        .frame(width: 14, height: 14)
-                        .overlay(
-                            Circle()
-                                .stroke(Color.gray, lineWidth: 2)
-                        )
-                    Text("No Goal")
-                }
-                HStack {
-                    Circle()
-                        .fill(Color("FailColor"))
-                        .frame(width: 16, height: 16)
-                    Text("Fail")
-                }
-                HStack {
-                    Circle()
-                        .fill(Color("ProgressColor"))
-                        .frame(width: 16, height: 16)
-                    Text("Small Progress")
-                }
-            }
-            .padding()
-            VStack(alignment: .leading) {
-                HStack {
-                    Circle()
-                        .fill(Color("AccentColor"))
-                        .frame(width: 16, height: 16)
-                    Text("Big Progress")
-                }
-                HStack {
-                    Circle()
-                        .fill(Color("SuccessColor"))
-                        .frame(width: 16, height: 16)
-                    Text("Success")
-                }
-                
-            }
-            .padding()
-        }
-    }
-}
-
 extension ProgressView {
     private func fetchGoals() {
         viewModel.allGoals = viewModel.fetchGoals()
